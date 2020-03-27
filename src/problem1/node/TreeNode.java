33dc9237904c4b0a5075995d0dd3554899f96fe8
rahul215 <rahul.saxena_cs18@gla.ac.in>
2020-03-27 22:25:53 +0530

    main class

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -6,5 +6,15 @@
  */
 package problem1.node;
 
-public class TreeNode {
+public class TreeNode<T> {
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
+    //public Object value;
+
+    // public Object value;
+    //public Object right;
+    public TreeNode(int value) {
+        this.value = value;
+    }
 }
